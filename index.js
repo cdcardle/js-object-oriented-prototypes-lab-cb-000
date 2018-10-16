e5ab364242db2409b52a2c3fa1d1249dc49ff4d3
Chris Cardle <cdcardle@gmail.com>
2018-10-16 18:13:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ function BoardMember(name, homeState, training){
   this.training = training
 }
 
-function User.prototype.veto = function(){
-  return 'No, I must disagree'
-}
-function User.prototype.approve = function(){
-  return 'You can do that!'
-}
-function User.prototype.doCharity = function(){
-  return "I like to help people."
-}
-
-function User.prototype.releasePressStatement = function(){
-  return "You will see great things from Scuber."
-}
-
-function User.prototype.sayHi = function(){
-  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-}
-
+// function User.prototype.veto = function(){
+//   return 'No, I must disagree'
+// }
+// function User.prototype.approve = function(){
+//   return 'You can do that!'
+// }
+// function User.prototype.doCharity = function(){
+//   return "I like to help people."
+// }
+//
+// function User.prototype.releasePressStatement = function(){
+//   return "You will see great things from Scuber."
+// }
+//
+// function User.prototype.sayHi = function(){
+//   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+// }
